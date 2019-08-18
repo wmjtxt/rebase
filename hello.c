0bc065cc18a6d1cd9e87e5180b4227c30c9c3c06
wmjtxt <972213032@qq.com>
2019-08-18 12:45:32 +0800

    hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+ ///
+ /// @file    :hello.c
+ /// @author  :wmjtxt(972213032@qq.com)
+ /// @date    :2019-08-18 12:45:03(NewYork time)
+ /// @quote   :
+ ///
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	printf("hello\n");
+	return 0;
+}
