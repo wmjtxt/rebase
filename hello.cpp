73d079903aba3d5067d31cb3180fb2664b7f70e7
wmjtxt <972213032@qq.com>
2019-08-18 12:50:08 +0800

    hello.cpp hello

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+ ///
+ /// @file    :hello.cpp
+ /// @author  :wmjtxt(972213032@qq.com)
+ /// @date    :2019-08-18 12:49:42
+ /// @quote   :
+ ///
+ 
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "hello" << endl;
+	return 0;
+}
